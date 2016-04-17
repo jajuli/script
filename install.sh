4eb91ece87e0bfe0e3f2f0b8ab512754dfa8df35
Jajuli <jajuli@users.noreply.github.com>
2016-04-17 22:10:35 +0700

    Create install.sh
    wget https://raw.githubusercontent.com/arjitc/autoinstall-kloxomr/master/install.sh
    chmod +x install.sh
    sh install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo Starting Kloxo-MR install...
+read -p "press [enter] to start..."
+cd /tmp
+yum update -y
+yum install yum-utils yum-priorities vim-minimal subversion curl zip unzip -y
+yum install telnet -y
+setenforce 0
+echo ‘SELINUX=disabled’ > /etc/selinux/config
+wget https://github.com/mustafaramadhan/kloxo/raw/rpms/release/neutral/noarch/mratwork-release-0.0.1-1.noarch.rpm --no-check-certificate
+rpm -ivh mratwork-release-0.0.1-1.noarch.rpm
+yum clean all
+yum update mratwork-* -y
+yum install kloxomr7 -y
+sh /script/upcp
+
+read -p "Install done! Reboot VPS once done!"
